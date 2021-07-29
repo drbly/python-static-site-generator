a63a8756dfc30db2eece2504c291fd0a2fdd9114
Dakota Bly <dakota.bly@kinsaleins.com>
2021-07-29 11:34:15 -0400

    fix site build method

--- ssg/site.py
+++ ssg/site.py
@@ -8,12 +8,12 @@ class Site:
 
 
 def create_dir(self, path):
-    directory = f"{self.dest}/{path.relative_to(self.source)}"
+    directory = self.dest / path.relative_to(self.source)
     directory.mkdir(parents=True, exist_ok=True)
 
 
 def build(self):
     self.dest.mkdir(parents=True, exist_ok=True)
     for path in self.source.rglob("*"):
-        if path.is_directory():
+        if path.is_dir():
             self.create_dir(path)
