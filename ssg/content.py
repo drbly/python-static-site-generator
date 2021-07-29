cd6a293f060461a76ea0d24f3303be6593737ef0
Dakota Bly <dakota.bly@kinsaleins.com>
2021-07-29 16:34:29 -0400

    content with bugs

--- ssg/content.py
+++ ssg/content.py
@@ -5,10 +5,43 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
-    def load(self, cls, string):
-        _, fm, content = Content.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        data = metadata
+        self.data = {"content": content}
+
+    @property
+    def body(self):
+        return self.data["contents"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        return self.data.__iter__()
+
+    def __len__(self):
+        return self.data.__len__()
+
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key is not "content":
+                value = data[key]
+        return str(data)
