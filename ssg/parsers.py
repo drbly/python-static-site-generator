ac573ea14761f0f745cc5692303ba472ff299ce5
Dakota Bly <dakota.bly@kinsaleins.com>
2021-08-02 09:50:45 -0400

    parser subclasses bugs fixed

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -43,7 +43,7 @@ class MarkdownParser(Parser):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
 class ReStructuredTextParser(Parser):
@@ -52,5 +52,5 @@ class ReStructuredTextParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(html[path, dest, "html_body"])
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
